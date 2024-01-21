79eb2a1a7f1d6d28492bb48cc0bc35e72d2a7f5a
d-k-bo <47948262+d-k-bo@users.noreply.github.com>
2024-01-21 18:41:34 +0100

    Fix docs for `ope_encoder_get_option`/`ope_encoder_set_option`

--- opusenc-sys/src/lib.rs
+++ opusenc-sys/src/lib.rs
@@ -10,16 +10,20 @@ use std::ffi::c_int;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+/// Read an encoder option.
+///
 /// libopus/libopusenc uses C macros to get options.
 ///
-/// A `GET_$X(x)`  macro usually resolves to `GET_$X_REQUEST, x` where x is checked to be a valid pointer
+/// A `$PREFIX_GET_$X(x)` macro usually resolves to `$PREFIX_GET_$X_REQUEST, x` where x is checked to be a valid pointer
 #[inline]
 pub unsafe fn ope_encoder_get_option(enc: *mut OggOpusEnc, request: u32, x: *mut c_int) -> c_int {
     ope_encoder_ctl(enc, request as c_int, x)
 }
+/// Write an encoder option.
+///
 /// libopus/libopusenc uses C macros to get options.
 ///
-/// A `SET_$X(x)`  macro usually resolves to `GET_$X_REQUEST, x` where x is checked to be a valid i32
+/// A `$PREFIX_SET_$X(x)` macro usually resolves to `$PREFIX_SET_$X_REQUEST, x` where x is checked to be a valid i32
 #[inline]
 pub unsafe fn ope_encoder_set_option(enc: *mut OggOpusEnc, request: u32, x: c_int) -> c_int {
     ope_encoder_ctl(enc, request as c_int, x)
